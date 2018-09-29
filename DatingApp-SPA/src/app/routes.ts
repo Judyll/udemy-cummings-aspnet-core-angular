3f2df5563cc74ea92c0c5c20ef789e55972b1d78
Judyll Agan <judyll.agan@gmail.com>
2018-09-29 14:55:28 +0800

    Protecting multiple routes with a single route guard

--- DatingApp-SPA/src/app/routes.ts
+++ DatingApp-SPA/src/app/routes.ts
@@ -11,11 +11,22 @@ export const appRoutes: Routes = [
   // down to this list attempting to match to the url.  If nothing matches this path,
   // then it gonna use the wild card and it's gonna redirect to home.  So, the ordering
   // of these routes is important.
-  { path: 'home', component: HomeComponent },
-  { path: 'members', component: MemberListComponent, canActivate: [AuthGuard] },
-  { path: 'messages', component: MessagesComponent },
-  { path: 'list', component: ListComponent },
+
+  // Instead of using 'home', we will just use '' so that when a user paste the url
+  // localhost:4200, then the user will be redirected to the home component
+  { path: '', component: HomeComponent },
+  // Add a dummy route so that we can guard multiple child routes
+  {
+    path: '',
+    runGuardsAndResolvers: 'always',
+    canActivate: [AuthGuard],
+    children: [
+      { path: 'members', component: MemberListComponent },
+      { path: 'messages', component: MessagesComponent },
+      { path: 'list', component: ListComponent }
+    ]
+  },  
   // redirect to the full path of the home URL 
-  { path: '**', redirectTo: 'home', pathMatch: 'full' }
+  { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
 
