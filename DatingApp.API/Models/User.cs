8ddad7553dc1a9d49fb4d5e4fe09ef00ae73c5c7
Judyll Agan <judyll.agan@gmail.com>
2018-09-29 15:31:15 +0800

    Extending the User model

--- DatingApp.API/Models/User.cs
+++ DatingApp.API/Models/User.cs
@@ -1,4 +1,7 @@
-﻿namespace DatingApp.API.Models
+﻿using System;
+using System.Collections.Generic;
+
+namespace DatingApp.API.Models
 {
     public class User
     {
@@ -9,5 +12,27 @@
         public byte[] PasswordHash { get; set; }
 
         public byte[] PasswordSalt { get; set; }
+
+        public string Gender { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public string KnownAs { get; set; }
+
+        public DateTime Created { get; set; }
+
+        public DateTime LastActive { get; set; }
+
+        public string Introduction { get; set; }
+
+        public string LookingFor { get; set; }
+
+        public string Interest { get; set; }
+
+        public string City { get; set; }
+
+        public string Country { get; set; }
+
+        public ICollection<Photo> Photos { get; set; }
     }
 }
