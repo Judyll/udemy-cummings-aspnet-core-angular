c247412b00b9c7ab25965ab93523af8ab1fa52dd
Judyll Agan <judyll.agan@gmail.com>
2018-12-08 15:25:36 +0800

    #207 Adding the edit roles method in our API

--- DatingApp.API/Controllers/AdminController.cs
+++ DatingApp.API/Controllers/AdminController.cs
@@ -4,6 +4,9 @@ using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using DatingApp.API.Models;
+using DatingApp.API.Dtos;
 
 namespace DatingApp.API.Controllers
 {
@@ -13,19 +16,21 @@ namespace DatingApp.API.Controllers
     {
         #region Fields
         private readonly DataContext _context;
+        private readonly UserManager<User> _userManager;
         #endregion
 
         #region Ctor
-        public AdminController(DataContext context)
+        public AdminController(DataContext context, UserManager<User> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
         #endregion
 
         #region Public Methods
 
         [Authorize(Policy = "RequireAdminRole")]
-        // The route path is usersWithRoles
+        // The route path is userswithroles
         [HttpGet("userswithroles")]
         public async Task<IActionResult> GetUserWithRoles()
         {
@@ -42,8 +47,41 @@ namespace DatingApp.API.Controllers
             return Ok(userList);
         }
 
+        [Authorize(Policy = "RequireAdminRole")]
+        [HttpPost("editroles/{userName}")]
+        public async Task<IActionResult> EditRoles(string userName, RoleEditDto roleEditDto)
+        {
+            var user = await _userManager.FindByNameAsync(userName);
+
+            var userRoles = await _userManager.GetRolesAsync(user);
+
+            // There is a chance that the user will be removed from all roles.
+            // If they are, then we are just going to create a new empty object
+            // of type string array
+            var selectedRoles = roleEditDto.RoleNames ?? new string[] { };
+
+            // We need to add in the user to roles that they are not
+            // yet a member of
+            var result = await _userManager.AddToRolesAsync(user,
+                selectedRoles.Except(userRoles));
+
+            if (!result.Succeeded)
+                return BadRequest("Failed to add to roles.");
+
+            // We need to remove in the roles that has been deselected
+            // from the list in the SPA
+            result = await _userManager.RemoveFromRolesAsync(user, 
+                selectedRoles.Except(selectedRoles));
+
+            if (!result.Succeeded)
+                return BadRequest("Failed to remove the roles.");
+
+            // Return the new list of roles the user is a member of
+            return Ok(await _userManager.GetRolesAsync(user));
+        }  
+
         [Authorize(Policy = "ModeratePhotoRole")]
-        // The route path is photosForModeration
+        // The route path is photosformoderation
         [HttpGet("photosformoderation")]
         public IActionResult GetPhotosForModeration()
         {
